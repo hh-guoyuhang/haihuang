ae64ea43b973f85dc0384e58cbcefdd525bc082c
郭宇航 <951770283@qq.com>
2019-08-20 22:07:00 +0800

    【add】新建实体类

---
+++ haihuang-modul/haihuangOfficialWebsite/src/main/java/haihuanguser/controller/HaihuangController.java
@@ -0,0 +1,21 @@
+package haihuanguser.controller;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HaihuangController {
+    private static Logger logger = Logger.getLogger(HaihuangController.class);
+
+    @RequestMapping("")
+    public String index()
+    {
+        return "index";
+    }
+
+
+
+}
